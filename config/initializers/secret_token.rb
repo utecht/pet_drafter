643db5ea0aa688989a140785f65cab31113aa3a7
Joseph Utecht <thenmal@gmail.com>
2013-03-28 13:16:19 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PetDrafter::Application.config.secret_token = 'a75e20cf42caab20d6c0332cbcdfa8f94b532e683558e602f09fdcd44b01a46bd9fd3cb00f45e4acc9a1d5cf9843124761c1722f3cd103aee3c06d9e380415d2'
