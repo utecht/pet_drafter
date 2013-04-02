f6757a89f4f3a01bd480c561795107006bac2060
Joseph Utecht <thenmal@gmail.com>
2013-04-02 13:00:40 -0500

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ PetDrafter::Application.routes.draw do
   devise_scope :user do
     root :to => "home#index"
     match '/update' => 'home#update'
-    match ':controller/:action/:id/:pet_id'
+    match '/teams/:id/:action/:pet_id' => 'teams'
     match '/lobbies/challenge/'
     match '/lobbies/accept/'
     match '/games/:id' => 'game#show'
-    match '/games/:id/:action/:pet_id'
+    match '/games/:id/:action/:pet_id' => 'games'
     resources :characters, :teams, :lobbies
   end
 end
