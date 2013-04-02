9991439cb99b2f3db96f2f62ea766696393c47c6
Joseph Utecht <thenmal@gmail.com>
2013-04-02 12:02:02 -0500

    passing status text

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -14,8 +14,20 @@ class GameController < ApplicationController
   # 11 - opponent pick
     def show
         @game = Game.find(params[:id])
-        @user_pets = @game.game_pets.joins(:pet).where('pets.user_id = ?',  @game.user.id)
-        @opponent_pets = @game.game_pets.joins(:pet).where('pets.user_id = ?',  @game.opponent.id)
+        @user_pets = @game.game_pets.joins(:pet).where('pets.user_id = ?', @game.user.id)
+        @opponent_pets = @game.game_pets.joins(:pet).where('pets.user_id = ?',@game.opponent.id)
+        @waiting = {0 => "#{@game.user.email} ban",
+          1 => "#{@game.opponent.email} ban",
+          2 => "#{@game.user.email} pick",
+          3 => "#{@game.opponent.email} pick",
+          4 => "#{@game.opponent.email} ban",
+          5 => "#{@game.user.email} ban",
+          6 => "#{@game.opponent.email} pick",
+          7 => "#{@game.user.email} pick",
+          8 => "#{@game.user.email} ban",
+          9 => "#{@game.opponent.email} ban",
+          10 => "#{@game.user.email} pick",
+          11 => "#{@game.opponent.email} pick"}
     end
 
     def ban_pet
